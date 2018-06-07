06104aa1ae5420c7ac2ae6705cb8d0439065ede4
Aaron <aaaa>
2018-06-07 13:29:04 -0700

    remoce locations

--- src/apollo/gql/Tag.hx
+++ src/apollo/gql/Tag.hx
@@ -5,7 +5,7 @@ import graphql_js.language.Ast;
 class Tag{
 
     static public function gql(graphql:String):DocumentNode{
-        var parser = new graphql.parser.Parser(graphql);
+        var parser = new graphql.parser.Parser(graphql, { noLocation:true });
         return {kind:"Document", definitions:parser.document.definitions};
     }
 }
